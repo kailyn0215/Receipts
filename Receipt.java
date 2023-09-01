b0c7ec823b5db379cd57d9c259fdda6b5590bcea
kailyn0215 <brownkai24@7hills.org>
2023-09-01 12:37:13 -0400

    pasted

--- Receipt.java
+++ Receipt.java
@@ -1,1 +1,34 @@
-public class Receipt{}
+// Receipt.java
+
+import java.util.Scanner;
+
+public class Receipt
+{
+    public static void main(String[] args)
+    {
+        int numToenail;
+        double priceToenail = 9.95;
+        String descToenail = "Luxury Clipped Toenails";
+        int numFingernail;
+        double priceFingernail = ;
+        String descFingernail;
+        int numBread;
+        double priceBread;
+        String descBread;
+        int numEscargot;
+        double priceEscargot;
+        String descEscargot;
+        int numGarlic;
+        double priceGarlic;
+        String descGarlic;
+
+        System.out.println("Welcome to my store!!! Please either buy something or get out :|");
+        System.out.println("Please go through the prompts and enter how many of each item you would like to buy.");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many luxury clipped toenails would you like to buy? ($9.95 each) ");
+        toenail = scan.nextInt();
+        System.out.print("How many finely chopped ($9.95 each) ");
+        toenail = scan.nextInt();
+
+    }
+}
