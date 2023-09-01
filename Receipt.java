fb104ee79df2762411f5d9e8c17b1c94e40cc393
kailyn0215 <brownkai24@7hills.org>
2023-09-01 12:41:52 -0400

    added price + desc

--- Receipt.java
+++ Receipt.java
@@ -9,20 +9,24 @@ public class Receipt
         int numToenail;
         double priceToenail = 9.95;
         String descToenail = "Luxury Clipped Toenails";
-        int numFingernail;
-        double priceFingernail = ;
-        String descFingernail;
-        int numBread;
-        double priceBread;
-        String descBread;
-        int numEscargot;
-        double priceEscargot;
-        String descEscargot;
-        int numGarlic;
-        double priceGarlic;
-        String descGarlic;
 
-        System.out.println("Welcome to my store!!! Please either buy something or get out :|");
+        int numFingernail;
+        double priceFingernail = 5.99;
+        String descFingernail = "Finely Chopped Fingernails";
+
+        int numBread;
+        double priceBread = 1.25;
+        String descBread = "Aged Moldy Bread";
+
+        int numEscargot;
+        double priceEscargot = 89.99;
+        String descEscargot = "Delicately Sizzled Escarcot";
+
+        int numGarlic;
+        double priceGarlic = 69.69;
+        String descGarlic = "Beautifully Stinky Garlic";
+
+        System.out.println("Welcome to my store!!! Please either buy something or get out :)");
         System.out.println("Please go through the prompts and enter how many of each item you would like to buy.");
         Scanner scan = new Scanner(System.in);
         System.out.print("How many luxury clipped toenails would you like to buy? ($9.95 each) ");
